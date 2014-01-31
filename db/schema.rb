2b7115fb6fc5712dddc5f91c0e1fede4ec5b18d2
Mary Dizon <mdizon1@gmail.com>
2014-01-30 20:30:49 -0800

    migrate memberships table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140128044417) do
+ActiveRecord::Schema.define(version: 20140131042813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "households", force: true do |t|
     t.string "name"
+  end
+
+  create_table "memberships", force: true do |t|
+    t.integer  "member_id"
+    t.integer  "household_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
