1cfd188582bc9907914b38135d6549b59205c529
Mary Dizon <mdizon1@gmail.com>
2014-03-05 22:01:18 -0800

    Implement household and todo methods

--- app/models/household.rb
+++ app/models/household.rb
@@ -1,4 +1,17 @@
 class Household < ActiveRecord::Base
   has_many :memberships
   has_many :members, :through => :memberships, :source => :user
+  has_many :todos
+
+  def create_todo(description, creator)
+    raise ArgumentError, "No description provided" if description.blank?
+    raise ArgumentError, "No creator provided" unless creator
+    Todo.create(:household => self, :description => description, :creator => creator)
+  end
+
+  def add_member(user)
+  end
+
+  def remove_member(user)
+  end
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,23 @@
 class Todo < ActiveRecord::Base
   belongs_to :household
+  belongs_to :creator, :foreign_key => :creator_id, :class_name => "User"
+  belongs_to :completor, :foreign_key => :completor_id, :class_name => "User"
+
+  def complete!(completor)
+    raise "A completor was not provided to complete! the todo" unless completor
+    self.completed_at = Time.now if  self.completed_at == nil
+    self.completor = completor if self.completor == nil
+  end
+
+  def complete?
+    #IMPLEMENT ME!!!
+    if(self.completed_at != nil && self.completor != nil)
+      return true
+    end
+  end
+
+  def uncomplete!
+    #IMPLEMENT ME
+    raise NotImplementedError
+  end
 end
