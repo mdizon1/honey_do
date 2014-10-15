1dc1febf159751e2b8fec2c05f056f49256e8947
Mary Dizon <mdizon1@gmail.com>
2014-10-14 20:46:18 -0700

    Send user to the household path if they're logged in and goto the root url

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,14 @@
 class WelcomeController < ApplicationController
+  before_filter :goto_household, :only => :index
+
   def index
     #nothing here yet
+  end
+
+  private
+  def goto_household
+    if user_signed_in? && household = current_user.households.first
+      return redirect_to household_path
+    end
   end
 end
