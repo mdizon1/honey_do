43dc2bcb9bcc845ca01b96692f43db1f73a5f3ce
momochi000 <momochi000@gmail.com>
2016-03-15 15:03:03 -0700

    Add auth token column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309062037) do
+ActiveRecord::Schema.define(version: 20160315220029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 20150309062037) do
     t.datetime "updated_at"
     t.string   "first_name",             limit: 255, default: ""
     t.string   "last_name",              limit: 255, default: ""
+    t.string   "authentication_token",               default: ""
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
