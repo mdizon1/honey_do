ab92a5d2428f5709ba743587a3f1c47070bcbfd6
momochi <momochi000@gmail.com>
2016-06-09 15:45:52 -0700

    Build a config file for launching webpack from a remote dev server

---
+++ config/webpack/remote_development.config.js
@@ -0,0 +1,26 @@
+// NOTE: Use this file when need to access bundle.js from a remote machine
+//   Also need to update the webpack dev server script tag(s) in application layout
+//   Also need to launch webpack with --host 0.0.0.0
+//   TODO: let these read from an environment variable so I can launch the 
+//   remote server with a single command
+var webpack = require('webpack');
+var _ = require('lodash');
+var config = require('./main.config.js');
+var devServerPort = '8080';
+var outputHost = '<ip-of-server>'; // REPLACE: 
+
+config = _.merge(config, {
+  debug: true,
+  displayErrorDetails: true,
+  outputPathinfo: true,
+  devtool: 'cheap-module-eval-source-map',
+  output: {
+    publicPath: '//' + outputHost + ':' + devServerPort + '/webpack/'
+  },
+  devServer: {
+    port: devServerPort,
+    headers: { 'Access-Control-Allow-Origin': '*' }
+  }
+});
+
+module.exports = config;
