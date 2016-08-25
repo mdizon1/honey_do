995105f7ef9f85b51355e25afdd3bb0d523bb027
momochi <momochi000@gmail.com>
2016-08-25 14:32:37 -0700

    Build a logger in javascript for more convenient logging

---
+++ app/frontend/javascripts/util/Logger.js
@@ -0,0 +1,27 @@
+const log = (msg, outArgs=[]) => {
+  _output("LOG: "+msg, 1, outArgs);
+}
+const debug = (msg, outArgs=[]) => {
+  _output("DEBUG: "+msg, 2, outArgs);
+}
+const dev = (msg, outArgs=[]) => {
+  _output("DEV_DEBUG: "+msg, 3, outArgs);
+}
+
+function _output(msg, logLevel, outObjects){
+  if(!window.logLevel) {
+    window.logLevel = 0;
+  }
+
+  if(window.logLevel < logLevel) {
+    return;
+  }
+
+  if(outObjects.length == 0) {
+    console.log(msg);
+  }else{
+    console.log(msg, outObjects);
+  }
+}
+
+export { log, debug, dev };
