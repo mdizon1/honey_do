d7c6e1ec32af29d2d906466570fd994d9dad32bc
momochi <momochi000@gmail.com>
2016-09-19 17:29:21 -0700

    Add a cucumber step for saving a screenshot

--- features/step_definitions/debug_steps.rb
+++ features/step_definitions/debug_steps.rb
@@ -6,6 +6,10 @@ When /^I open the page$/ do
   save_and_open_page
 end
 
+When /^I save a screenshot$/ do
+  save_and_open_screenshot
+end
+
 When /^I wait (\d+) seconds?$/ do |num_secs|
   sleep(num_secs.to_i)
 end
