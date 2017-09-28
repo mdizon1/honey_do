010f3290d4d7728f2d913694bffa272a940978dd
momochi <momochi000@gmail.com>
2017-09-27 18:10:33 -0700

    Fix rendering of drag placeholder for touch

--- app/javascript/packs/honey_do/components/TodoDragLayer.js
+++ app/javascript/packs/honey_do/components/TodoDragLayer.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react'
+import Checkbox from 'material-ui/Checkbox/Checkbox'
 import { ItemTypes } from './../constants/ItemTypes'
 import { DragLayer } from 'react-dnd'
 
@@ -37,6 +38,25 @@ const layerStyles = {
   height: '100%'
 };
 
+const _renderNotes = (todo) => {
+  let notes_class;
+  notes_class = "todo-item-notes";
+  notes_class += " todo-item-notes-truncated";
+  return ( 
+    <div className={notes_class}>
+      { todo.notes }
+    </div>
+  )
+}
+
+const _renderTitle = (todo) => {
+  return (
+    <div className='todo-item-title'>
+      <h4> { todo.title } </h4>
+    </div>
+  )
+}
+
 class TodoDragLayer extends Component {
   render() {
     const { item, itemType, isDragging } = this.props;
@@ -48,8 +68,24 @@ class TodoDragLayer extends Component {
           className="todo-item todo-item-drag-placeholder"
           style={layerStyles}
         > 
-          <div style={getItemStyles(this.props)}>
-            <h4> Hey! Put me down! </h4>
+
+          <div className="todo-item">
+            <div className="row">
+              <div className="col-2">
+                <div className={"todo-item-checkbox" + (item.isCompleted ? ' checkbox-checked' : '')}>
+                  <Checkbox
+                    checked={item.isCompleted}
+                    disabled={item.isCompleted && !item.permissions.canUncomplete}
+                  />
+                </div>
+              </div>
+              <div className="col-10">
+                <div className={"todo-item-content"}>
+                  { _renderTitle(item) }
+                  { _renderNotes(item) }
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       );
@@ -58,3 +94,6 @@ class TodoDragLayer extends Component {
 }
 
 export default DragLayer(collect)(TodoDragLayer);
+//          <div style={getItemStyles(this.props)}>
+//            <h4> Hey! Put me down! </h4>
+//          </div>
