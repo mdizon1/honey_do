8365a300d3481bc45c313efb9eb8bc630d4d7a6c
momochi <momochi000@gmail.com>
2017-10-24 17:47:50 -0700

    Build stub of seed tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+seed_tags = [
+  'Target', 'Whole Foods', 'Andronicos', 'Sprouts', 'Costco'
+]
+
+seed_tags.each do |curr_tag|
+  new_tag = TagTitle.find_or_create_by(:title => curr_tag)
+  unless(new_tag.persisted?)
+    puts("\tLOG: SEED: created a new tag for --> #{curr_tag}");
+  else
+    puts("\tLOG: SEED: tag was not created --> #{curr_tag} because --> #{new_tag.errors.full_messages.join(",")}");
+  end
+end
