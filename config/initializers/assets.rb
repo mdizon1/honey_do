11937784358a78bc94c0b7e3e48d1098f039fda7
momochi <momochi000@gmail.com>
2018-05-08 16:22:23 -0700

    Add placeholder image to cover hero vid
    while the hero vid loads, or if video not supported

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,4 @@
 #Rails.application.config.assets.configure do |env|
 #  env.unregister_postprocessor 'application/javascript', Sprockets::SafetyColons
 #end
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'videos')
