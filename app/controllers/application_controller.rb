f51ca56687d9d9f0899fff0ca2402ede4cb9f0e0
momochi <momochi000@gmail.com>
2018-05-09 01:29:58 -0700

    Add signin/signup modal to nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :resource_name, :resource, :devise_mapping, :resource_class
 
   def after_sign_in_path_for(resource)
     if resource.class == User
@@ -30,5 +31,21 @@ class ApplicationController < ActionController::Base
     rescue
       return render :json => {:message => 'Your authentication token is invalid, it may have expired or you may have logged in from another location.  Please log out, then log in and try again.'}, :status => 403
     end
+  end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def resource_class
+    User
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
   end
 end
