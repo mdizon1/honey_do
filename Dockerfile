d0c9d6ceee738822fc75fe13ad3354c7239e7c70
momochi <momochi000@gmail.com>
2018-05-11 17:17:28 -0700

    Oops, fix line in Dockerfile used for debugging

--- Dockerfile
+++ Dockerfile
@@ -63,7 +63,7 @@ RUN ./bin/yarn install
 # This part is needed in production for heroku. Unfortunately, it's getting
 # in the way in development
 ARG ASSET_HOST
-RUN bin/rake ASSET_HOST=${ASSET_HOST} RAILS_ENV=production assets:precompile
+RUN bin/rake ASSET_HOST=${ASSET_HOST} assets:precompile
 
 RUN useradd -m myuser
 USER myuser
--- config/webpack/loaders/sass.js
+++ config/webpack/loaders/sass.js
@@ -18,23 +18,23 @@ if(env.NODE_ENV === 'production') {
         }
       }
     ]
-  })
+  });
 }else{
-
-use = ExtractTextPlugin.extract({
-  fallback: 'style-loader',
-  use: [
-    { loader: 'css-loader', options: { minimize: env.NODE_ENV === 'production', importLoaders: 1, sourceMap: true } },
-    { loader: 'postcss-loader', options: { sourceMap: true } },
-    'resolve-url-loader',
-    { loader: 'sass-loader', options: { sourceMap: true } },
-    { loader: 'sass-resources-loader', 
-      options: {
-        resources: path.join(__dirname, "../", "../", "../", "app", "javascript", "styles", "common.scss")
+  use = ExtractTextPlugin.extract({
+    fallback: 'style-loader',
+    use: [
+      { loader: 'css-loader', options: { importLoaders: 1, sourceMap: true } },
+      { loader: 'postcss-loader', options: { sourceMap: true } },
+      'resolve-url-loader',
+      { loader: 'sass-loader', options: { sourceMap: true } },
+      { loader: 'sass-resources-loader', 
+        options: {
+          resources: path.join(__dirname, "../", "../", "../", "app", "javascript", "styles", "common.scss")
+        }
       }
-    }
-  ]
-})
+    ]
+  });
+}
 
 //  use = [
 //    { loader: 'style-loader', options: { sourceMap: true } },
@@ -48,8 +48,6 @@ use = ExtractTextPlugin.extract({
 //      }
 //    }
 //  ]
-//}
-
 
 module.exports = {
   test: /\.(scss|sass|css)$/i,
