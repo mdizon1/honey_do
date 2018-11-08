9a78aaf72e3aced281b62fe8a782bd16ab6cf65f
Zachery Chin <zchin@sproutpost.net>
2018-11-08 11:27:03 -0800

    Prevent open new todo when press c in search field
    Create an empty hotkey handler for the search field itself
    I think this is an issue when using material ui because supposedly being
    in a text field or textarea should disable hotkeys or mousetrap by
    default

--- app/javascript/honey_do/components/SearchTodos.js
+++ app/javascript/honey_do/components/SearchTodos.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TextField from 'material-ui/TextField'
+import { HotKeys } from 'react-hotkeys'
 
 const renderSearchLabel = () => {
   return (
@@ -10,23 +11,33 @@ const renderSearchLabel = () => {
   )
 }
 
+const keyMap = {
+  newTodo: 'c'
+}
+
+const keyHandlers = {
+  'newTodo': () => {}
+}
+
 const SearchTodos = (props) => {
   return (
     <div className="todo-search-wrap">
-      <TextField
-        className="todo-search"
-        label={renderSearchLabel()}
-        type="search"
-        multiline
-        fullWidth={true}
-        onChange={props.onChange('searchValue')}
-        value={props.value}
-      >
-      </TextField>
-      <i
-        className="todo-search-clear-btn fa fa-times"
-        onClick={props.onClear}
-      ></i>
+      <HotKeys keymap={keyMap} handlers={keyHandlers}>
+        <TextField
+          className="todo-search"
+          label={renderSearchLabel()}
+          type="search"
+          multiline
+          fullWidth={true}
+          onChange={props.onChange('searchValue')}
+          value={props.value}
+        >
+        </TextField>
+        <i
+          className="todo-search-clear-btn fa fa-times"
+          onClick={props.onClear}
+        ></i>
+      </HotKeys>
 
     </div>
   )
